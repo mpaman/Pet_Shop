5292a99fe03af5d384f8aa76da0640cf00a0c317
mpaman <namusthai12345@gmail.com>
2025-01-14 23:56:47 +0700

    unit-test-

--- backend/entity/test/storeimage_test.go
+++ backend/entity/test/storeimage_test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"testing"
-	"fmt"
+	// "fmt"
 	"github.com/mpaman/petshop/entity"
 	"github.com/asaskevich/govalidator"
 	. "github.com/onsi/gomega"
@@ -11,65 +11,30 @@ import (
 func TestStoreImage(t *testing.T){
 	g := NewGomegaWithT(t)
 
-	t.Run("testUrl",func(t *testing.T){
+	t.Run("test-Url",func(t *testing.T){
 		image := entity.StoreImage{
 			ImageURL: "",
 			StoreID: 1,
 		}
 		ok,err := govalidator.ValidateStruct(image)
-		fmt.Println(err)
+		// fmt.Println(err)
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).NotTo(BeNil())
 
 		g.Expect(err.Error()).To(ContainSubstring("Image URL is required"))
 	})
+
+	t.Run("test-storeid",func(t *testing.T){
+		image := entity.StoreImage{
+			ImageURL: "https://example",
+			StoreID: 0,
+		}
+		ok,err := govalidator.ValidateStruct(image)
+		// fmt.Println(err)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(ContainSubstring("Store ID is required"))
+	})
 }
 
-// func TestStoreImageURLIsRequired(t *testing.T) {
-//     g := NewGomegaWithT(t)
-//     mockStore := entity.Store{
-//         Name: "Mock Store",
-//     }
-
-//     storeImage := entity.StoreImage{
-//         ImageURL: "", 
-//         Store:    mockStore,
-//     }
-
-//     err := storeImage.Validate()
-//     g.Expect(err).NotTo(BeNil())
-//     g.Expect(err.Error()).To(Equal("Image URL is required"))
-// }
-
-// func TestStoreImageURLInvalidFormat(t *testing.T) {
-// 	g := NewGomegaWithT(t)
-
-// 	mockStore := entity.Store{
-// 		Name: "Mock Store",
-// 	}
-
-// 	storeImage := entity.StoreImage{
-// 		ImageURL: "invalid-url", // Invalid
-// 		Store:    mockStore,
-// 	}
-
-// 	err := storeImage.Validate()
-// 	g.Expect(err).NotTo(BeNil())
-// 	g.Expect(err.Error()).To(Equal("Invalid URL format"))
-// }
-
-// func TestStoreImageValidURL(t *testing.T) {
-// 	g := NewGomegaWithT(t)
-
-// 	mockStore := entity.Store{
-// 		Name: "Mock Store",
-// 	}
-
-// 	storeImage := entity.StoreImage{
-// 		ImageURL: "https://example.com/image.jpg", // Valid
-// 		Store:    mockStore,
-// 	}
-
-// 	err := storeImage.Validate()
-// 	g.Expect(err).To(BeNil())
-// }
